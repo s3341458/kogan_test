7ea7c97b536ec692d4b58cb1b2b1e10fd9973191
chengyu <chengyu0316@gmail.com>
2019-01-31 21:02:28 +1100

    extract the weights on very response reduce a little bit load for memory

--- flask_api_consumer/api.py
+++ flask_api_consumer/api.py
@@ -10,19 +10,6 @@ def compose(*functions):
     return reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
 
 
-def retrieve_products(kwargs):
-    host = kwargs['host']
-    endpoint = kwargs['endpoint']
-    products_json = []
-    while True:
-        r = requests.get('{}{}'.format(host, endpoint))
-        endpoint = r.json()['next']
-        products_json.extend(r.json()['objects'])
-        if not endpoint:
-            break
-    return products_json
-
-
 def calculate_cubic_size(kwargs):
     return reduce(
         lambda x, y: x * y,
@@ -39,10 +26,27 @@ calculate_cubic_weight = compose(
 )
 
 
-calculate_aircon_cubic_weight_avg = compose(
-    lambda weights: sum(weights) / len(weights),
+extract_weights = compose(
     list,
     partial(map, calculate_cubic_weight),
     partial(filter, lambda x: x['category'] == 'Air Conditioners'),
-    retrieve_products,
+)
+
+
+def retrieve_weights(kwargs):
+    host = kwargs['host']
+    endpoint = kwargs['endpoint']
+    weights_list = []
+    while True:
+        r = requests.get('{}{}'.format(host, endpoint))
+        endpoint = r.json()['next']
+        weights_list.extend(extract_weights(r.json()['objects']))
+        if not endpoint:
+            break
+    return weights_list
+
+
+calculate_aircon_cubic_weight_avg = compose(
+    lambda weights: sum(weights) / len(weights),
+    retrieve_weights,
 )
